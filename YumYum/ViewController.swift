9e20a9667309ce403e0725c2abe92d019200186f
wspaleniak <w.spaleniak@o2.pl>
2022-10-10 13:21:59 +0200

    #1 commit - start project

---
+++ YumYum/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  YumYum
+//
+//  Created by Wojciech Spaleniak on 07/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("viewDidLoad")
+    }
+    
+}
